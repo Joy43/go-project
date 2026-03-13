2c9f4bc5a08c45d7d2b066df2618538bdd2b2068
ssjoy43@gmail.com <ssjoy43@gmail.com>
2026-03-13 20:32:11 +0600

    ✨ feat: Update auth and user models

--- controllers/authController.go
+++ controllers/authController.go
@@ -10,7 +10,7 @@ import (
 
 
 
-// Register user
+// ------ Register user ------
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	_ = json.NewDecoder(r.Body).Decode(&user)
@@ -33,32 +33,32 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-// Login user
+// ------- Login user-----------
 func Login(w http.ResponseWriter, r *http.Request) {
 	var loginUser models.User
 	_ = json.NewDecoder(r.Body).Decode(&loginUser)
 
-	// Retrieve user from the database
+	// ----- Retrieve user from the database ---------
 	var user models.User
 	if err := config.DB.Where("email = ?", loginUser.Email).First(&user).Error; err != nil {
 		http.Error(w, "User not found", http.StatusUnauthorized)
 		return
 	}
 
-	// Compare the password
+	// --------- Compare the password ---------
 	if !utils.ComparePassword(loginUser.Password, user.Password) {
 		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
 		return
 	}
 
-	// Generate JWT token
+	// ------Generate JWT token ---------
 	token, err := utils.GenerateJWT(user.Username)
 	if err != nil {
 		http.Error(w, "Error generating token", http.StatusInternalServerError)
 		return
 	}
 
-	// Send the token to the client
+	// --------- Send the token to the client ---------
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
--- models/user.go
+++ models/user.go
@@ -8,4 +8,23 @@ type User struct {
 	Username string `gorm:"size:100;not null" json:"username"`
 	Email    string `gorm:"size:100;not null;unique" json:"email"`
 	Password string `gorm:"size:100;not null" json:"password"`
+	Role     string `gorm:"size:20;not null;default:'user'" json:"role"`
+	Posts    []Post `json:"posts"`
+	Comments []Comment `json:"comments"`
+}
+
+type Post struct {
+	gorm.Model
+	Title   string `gorm:"size:255;not null" json:"title"`
+	Content string `gorm:"type:text;not null" json:"content"`
+	UserID  uint   `json:"user_id"`
+	Comments []Comment `json:"comments"`
+}
+
+type Comment struct {
+	gorm.Model
+	Content string `gorm:"type:text;not null" json:"content"`
+	UserID  uint   `json:"user_id"`
+	PostID  uint   `json:"post_id"`
+	User    User   `json:"user"`
 }
